90c5f0ed6a5a44a7e60794808fa67b99e4efe030
wangruilong <kobewangrui@163.com>
2017-11-20 15:11:21 +0800

    传参修改状态

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <img src="./assets/logo.png">
     <b>王睿龙的第一个vuex状态count:{{$store.state.count}}</b>
-    <button @click="$store.commit('add')">count add</button>
-    <button @click="$store.commit('reduce')">count reduce</button>
+    <button @click="$store.commit('add',10)">count add</button>
+    <button @click="$store.commit('reduce',10)">count reduce</button>
     <router-view/>
   </div>
 </template>
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -3,15 +3,15 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 
-const state = {//在state可以添加状态
+const state = {//在state可以添加全局共享的状态，
     count:1
 }
 const mutations = {//mutations是固定的写法，用来改变state的值的放假，需要写在其中。
-    add(state){
-        state.count += 1;
+    add(state,n){
+        state.count += n;
     },
-    reduce(state){
-        state.count -= 1;
+    reduce(state,n){
+        state.count -= n;
     }
 }
 export default new Vuex.Store({
