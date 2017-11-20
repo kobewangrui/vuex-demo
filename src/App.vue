4f4c94af21a45fb8e47e280d6f36ab741a6ef874
wangruilong <kobewangrui@163.com>
2017-11-20 15:07:15 +0800

    是在改变state ，调用mutations里面的方法

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <img src="./assets/logo.png">
     <b>王睿龙的第一个vuex状态count:{{$store.state.count}}</b>
+    <button @click="$store.commit('add')">count add</button>
+    <button @click="$store.commit('reduce')">count reduce</button>
     <router-view/>
   </div>
 </template>
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -3,10 +3,18 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 
-const state = {
+const state = {//在state可以添加状态
     count:1
 }
-
+const mutations = {//mutations是固定的写法，用来改变state的值的放假，需要写在其中。
+    add(state){
+        state.count += 1;
+    },
+    reduce(state){
+        state.count -= 1;
+    }
+}
 export default new Vuex.Store({
-    state
+    state,
+    mutations,
 })
