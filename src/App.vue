ede0eb0b9de4197969a22c338adb7c2b2009eaec
wangruilong <kobewangrui@163.com>
2017-11-20 15:55:29 +0800

    action里面方法的两种触发方式

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,30 @@
   <div id="app">
     <img src="./assets/logo.png">
     <b>王睿龙的第一个vuex状态count:{{$store.state.count}}</b>
-    <button @click="$store.commit('add',10)">count add</button>
-    <button @click="$store.commit('reduce',10)">count reduce</button>
+    <button @click="$store.commit('add',10)">mutations同步修改：count add</button>
+    <button @click="$store.commit('reduce',10)">mutations同步修改：count reduce</button>
+    <br>
+    <button @click="addAction()">actions异步修改：count add</button>
+    <button @click="reduceAction()">actions异步修改：count reduce</button>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data(){return{}},
+  created(){
+    console.log(this.$store.commit)
+  },
+  methods:{
+    addAction(){
+      this.$store.dispatch('addAction');
+    },
+    reduceAction(){
+      this.$store.dispatch('reduceAction');
+    }
+  }
 }
 </script>
 
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -6,7 +6,7 @@ Vue.use(Vuex);
 const state = {//在state可以添加全局共享的状态，
     count:1
 }
-const mutations = {//mutations是固定的写法，用来改变state的值的放假，需要写在其中。
+const mutations = {//mutations是固定的写法，用来改变state的值的放假，需要写在其中。同步修改state状态
     add(state,n){
         state.count += n;
     },
@@ -14,7 +14,19 @@ const mutations = {//mutations是固定的写法，用来改变state的值的放
         state.count -= n;
     }
 }
+const actions = {//actions和mutations功能一样，不同得是actions是异步修改该state状态，actions提交的是multation而不是直接更改状态
+    addAction(context){
+        context.commit('add',n);//actions是可以调用mutations里面的方法的
+    },
+    reduceAction(context){
+        context.commit('reduce',n);
+    }
+}
+// Action通过store.dispatch方法触发
+// store.dispatch('addAction')
+
 export default new Vuex.Store({
     state,
     mutations,
+    actions,
 })
