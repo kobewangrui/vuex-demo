ffd11b60f84a90aa12ea8e063e060948337e475a
wangruilong <kobewangrui@163.com>
2020-07-01 15:26:42 +0800

    push

---
+++ src/components/index.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    -------------------------- index page -------------------------------
+    <indexchilds :propsData="fatherData" @emitFuns="parentMethods"></indexchilds>
+    <span @click="changeFatherData">改变fatherData</span>
+</div>
+</template>
+<script>
+import indexChild from '@/components/indexChild'
+    export default{
+        data(){
+            return{
+                fatherData:11111,
+
+            }
+        },
+        methods:{
+            parentMethods(msg){
+                this.fatherData = msg;
+                alert(`我是父组件的方法啊,并且还有子组件传递的参数:${msg}`)
+            },
+            changeFatherData(){
+                this.fatherData ++
+            },
+
+        },
+        components:{
+            'indexchilds':indexChild
+        }
+    }
+</script>
---
+++ src/components/indexChild.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    -------------------------- indexchild page -------------------------------
+    再child显示fatherData:{{propsData}}
+    <span @click="runFaterFun">点我调用父组件方法、子=>传值</span>
+</div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                childNum:1111111,
+            }
+        },
+        methods:{
+            runFaterFun(){
+                this.$emit('emitFuns',this.childNum)
+            },
+        },
+        props:['propsData'],
+        watch:{
+            'propsData':function(val,oldVal){
+                alert('在子组件监听到父组件props数据在变化')
+            }
+        },
+    }
+</script>
