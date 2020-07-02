169ba191ee8d8aaa6f9b8f19ed6210ce0858529a
wangruilong <kobewangrui@163.com>
2020-07-02 14:38:29 +0800

    deepClone

--- src/components/indexChild.vue
+++ src/components/indexChild.vue
@@ -12,6 +12,28 @@
                 childNum:1111111,
             }
         },
+        created(){
+            let a = {
+                b:3,
+                c:{
+                    d:1,
+                }
+            }
+
+            // 深拷贝与浅拷贝定义概念理解
+            // 深拷贝指单独开辟另外一块内存空间（新的引用类型），两者值的修改互不影响
+            // 浅拷贝并没单独开辟内存空间，两者值的修改会导致另一方改变，指针指向同一块内存地址（没有新的引用类型）
+
+
+            // let b = a;//简单的赋值指针还是指向a，修改值相互影响
+            let b = Object.assign({},a);//对象第一层是深拷贝（第一层值改变相互不会相互影响），第二层是是浅拷贝（对象第二层值改变会相互影响）
+            // let b = JSON.parse(JSON.stringify(a));//深拷贝  开辟一块新的内存空间，并非引用。各自修改互不影响
+            b.c.d = 4
+            b.b = 10
+            
+            console.log(JSON.stringify(a))
+            console.log(JSON.stringify(b))
+        },
         methods:{
             runFaterFun(){
                 this.$emit('emitFuns',this.childNum)
