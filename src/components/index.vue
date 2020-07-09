e0c605f879a810b6b6115f484e0435b929635870
wangruilong <kobewangrui@163.com>
2020-07-09 15:34:08 +0800

    三次握手

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,5 +1,13 @@
 <template>
 <div>
+    三次握手的目的：是为了确认双方都有收发数据的能力。（参考前后端【客户端、服务端】交互）
+        第一次： A->B，A发送消息给B,证明A有发消息的能力。
+        第二次： ->B && B->A，b收到消息并发给A,证明B有收消息，并且有发消息的能力。
+        第三次： A->B，a收到消息，证明A有收消息的能力。
+        二次握手达不到目的，四次多余。
+
+
+
     -------------------------- index page -------------------------------
     <indexchilds :propsData="fatherData" @emitFuns="parentMethods"></indexchilds>
     <span @click="changeFatherData">改变fatherData</span>
