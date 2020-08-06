53d4fe4e7db20bd17f01aca56dcb4f533a86891b
wangruilong <kobewangrui@163.com>
2020-08-06 09:30:41 +0800

    update

---
+++ src/pages/indextwo.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div id="printTest">
+        <b>王睿龙的第一个vuex状态count:{{$store.state.count}}</b>
+        <button @click="$store.commit('add',10)">mutations同步修改：count add</button>
+        <button @click="$store.commit('reduce',10)">mutations同步修改：count reduce</button>
+        <br>
+        <button @click="addAction(1000)">actions异步修改：count add</button>
+        <button @click="reduceAction(1000)">actions异步修改：count reduce</button>
+      </div>
+      <button v-print="'#printTest'">打印111111111111111111111111</button>
+      <button v-print="'#printTest'">2222222222222222222</button>
+      <button v-print="'#printTest'">3333333333333333333333</button>
+      <img src="@/assets/logo.png">
+      <button @click="changeNumber">watch|computed区别</button>
+      computed：会有缓存、用于多个数据的变化影响一个数据、会有getter   依赖数据变化而计算、改变  ---------和method相比不用调用，
+      watch:  监听数据、数据变化引发一些操作，类似一个数据的变化，触发一系列的操作或者影响多个数据
+	  <p>computed：{{change}}</p>
+	  <p>wath：{{computedORwatch}}</p>
+      <!-- pdf预览 -->
+      <!-- <pdf ref="pdf" :src="pdfUrl" style="width: 100%;"></pdf> -->
+	  
+      <button @click="changeHello">父组件改变helloData：changeHelloData</button>
+      hello子组件组件：
+      <hello :parentData="helloData" @emitFun="parentFun"/>
+	  子=>父数据：{{childData}}
+  </div>
+</template>
+<script>
+// import pdf from 'vue-pdf'
+import Hello from '@/components/HelloWorld'
+export default {
+  name: 'app',
+  data(){return{
+    pdfUrl:'',
+	computedORwatch:1,
+	helloData:1,
+	childData:'',
+  }},
+  created(){
+    this.$store.commit('reduce',10);
+    this.reduceAction(10);
+    // this.pdfUrl = pdf.createLoadingTask(pdfurl)
+  },
+  components:{
+      // pdf:pdf
+  },
+  methods:{
+	  parentFun(childMsg){
+		  this.childData = childMsg;
+		  alert(childMsg)
+	  },
+	changeHello(){
+		this.helloData++;
+	},
+    addAction(n){
+      this.$store.dispatch('addAction',n);
+    },
+    reduceAction(n){
+      this.$store.dispatch('reduceAction',n);
+    },
+	changeNumber(){
+		this.computedORwatch += 10;
+	},
+  },
+  components:{
+      'hello':Hello,
+  },
+  computed:{
+	  change(){
+		  return this.computedORwatch + 1;
+	  },
+  },
+  watch:{
+	  'computedORwatch':function(val,oldVal){
+		  val + 1
+	  }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/index'
+import Indexone from '@/pages/indexone'
+import Indextwo from '@/pages/indextwo'
 import store from '@/vuex/store'//引入store
 
 Vue.use(Router)
@@ -12,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index
-    }
+    },
+    {
+      path: '/pageOne',
+      name: 'indexOne',
+      component: Indexone
+    },
+    {
+      path: '/pageTwo',
+      name: 'indexTwo',
+      component: Indextwo
+    },
   ]
 })
