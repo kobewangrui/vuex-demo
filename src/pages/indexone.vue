39c783d3cac2a311796ab2b7d5c228e4e857a974
wangruilong <kobewangrui@163.com>
2020-09-22 14:54:35 +0800

    proxy更直观的列子

--- src/pages/indexone.vue
+++ src/pages/indexone.vue
@@ -68,6 +68,7 @@ export default{}
             name:'wrl',
             age:33,
         }
+        // 参数详解
         // get(target, propKey, receiver)
         // target：目标对象
         // propKey：属性名
@@ -92,5 +93,26 @@ export default{}
         })
         target.age = 6
         target.age
+
+        // proxy 举例
+        // var target = {
+        //     name: 'poetries'
+        // };
+        // var logHandler = {
+        //     get: function(target, key) {
+        //         console.log(`${key} 被读取`);
+        //         return target[key];
+        //     },
+        //     set: function(target, key, value) {
+        //         console.log(`${key} 被设置为 ${value}`);
+        //         target[key] = value;
+        //     }
+        // }
+        // var targetWithLog = new Proxy(target, logHandler);
+        
+        // targetWithLog.name; // 控制台输出：name 被读取
+        // targetWithLog.name = 'others'; // 控制台输出：name 被设置为 others
+        
+        // console.log(target.name); // 控制台输出: others
 
 </script>
